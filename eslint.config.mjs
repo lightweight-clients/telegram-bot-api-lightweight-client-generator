2303d8fa9c23e29f6cf4d4a3c2b09fb734028a58
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-07-04 11:34:17 +0200

    Use Eta for templating, fix eslint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
